f0535767d4546661098820990488fa523fd8cf08
Fausto E. Rodriguez <faustoerv@gmail.com>
2023-10-10 20:49:15 -0400

    Especialidades

--- src/components/specialties/specialties.ts
+++ src/components/specialties/specialties.ts
@@ -1,30 +1,32 @@
-import {BaseComponent} from "../baseComponent";
+import { BaseComponent } from "../baseComponent";
 import data from "../../data/specialties.json";
 import Specialty from "../../ts/interfaces/specialty";
 import SpecialtyCard from "./specialtyCard";
 
 export default class Specialities extends BaseComponent {
   constructor() {
-    const template = /*html */ `    
-    <div class="row text-center mt-3"><h1>Especialidades</h1></div>
-    <div id="specialty-cards" class="row"></div>
+    const template = /*html */ `
+        <div class="container-fluid">
+            <div class="row text-center mt-3"><h1>Especialidades</h1></div>
+            <div id="specialty-cards" class="row"></div>
+        </div>    
     `;
 
     super(template);
 
     document.querySelector<HTMLDivElement>("#specialties")!.innerHTML =
-        this.getElement();
+      this.getElement();
 
     const specialties = getSpecialties();
 
     const specialtyCardsSection =
-        document.querySelector<HTMLDivElement>("#specialty-cards")!;
+      document.querySelector<HTMLDivElement>("#specialty-cards")!;
 
     specialties.forEach((specialty) => {
       const card = new SpecialtyCard(
-          specialty.text,
-          `./public/images/${specialty.image}`,
-          specialty.description
+        specialty.text,
+        `./public/images/${specialty.image}`,
+        specialty.description,
       );
 
       specialtyCardsSection.innerHTML += card.getElement();
--- src/components/specialties/specialtyCard.ts
+++ src/components/specialties/specialtyCard.ts
@@ -5,12 +5,13 @@ export default class SpecialtyCard extends BaseComponent {
     const template =
       /*html */
       `
-    <div class="col-12 col-lg-3">
+    <div class=" col-12 col-md-6 col-lg-3">
       <div class="card">
-        <img
-         src="${img}"
+        <img           
+           src="${img}"   
                 class="card-img-top"
-              />
+              />          
+              
               <div class="card-body">
                 <h5 class="card-title">${title}</h5>
                 <p>
