19869c95a988bf8ad12cce9fc0aac2737b2b1c16
Fausto E. Rodriguez <faustoerv@gmail.com>
2023-10-17 20:50:41 -0400

    log in form

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,7 +1,7 @@
 import { BaseComponent } from "../baseComponent";
 import data from "../../data/header.json";
 import { HeaderItem } from "../../ts/interfaces/headerItem.ts";
-
+import Swal from "sweetalert2";
 export default class Header extends BaseComponent {
   constructor() {
     const template =
@@ -28,17 +28,52 @@ export default class Header extends BaseComponent {
         <ul class="navbar-nav flex-row flex-wrap ms-md-auto">
           <li class="nav-item">
             <span class="nav-link active" aria-current="page" href="#"
-              >User Name</span
+              >User Name </span
             >
           </li>
-          <button class="btn btn-primary">Ingresar</button>
         </ul>
+        
+          <button id="loginBtn" type="button" class="btn btn-primary"><span class="m-2">Ingresar</span><i class="fa-solid fa-right-to-bracket"></i></button>
       </div>
     </nav>
   </header>`;
 
     super(template, "#header");
+    this.addListener();
   }
+
+  private addListener = () => {
+    const btnLogin = document.querySelector<HTMLButtonElement>("#loginBtn");
+    if (btnLogin) {
+      btnLogin.addEventListener("click", () => {
+        Swal.fire({
+          title: "Ingresar",
+          html: this.getLoginForm(),
+          focusConfirm: false,
+          confirmButtonText: "Ingresar",
+          preConfirm: () => {
+            return false;
+          },
+        });
+      });
+    }
+  };
+
+  private getLoginForm = () => {
+    return `
+    <span id="login-form-error" class="text-danger"></span>
+    <input id="swal-input1" class="swal2-input"  placeholder="Usuario">
+    <input id="swal-input2" class="swal2-input" type="password" placeholder="Contraseña">
+    <div class="swal2-radio">
+        <label>
+            <input type="radio" name="swal2-radio" value="1"><span class="swal2-label">Paciente</span>
+        </label>
+        <label>
+            <input type="radio" name="swal2-radio" value="2"><span class="swal2-label">Doctor</span>
+        </label>
+    </div>
+    `;
+  };
 }
 
 const getHeaderItems = () => {
@@ -49,8 +84,8 @@ const getHeaderItems = () => {
   headerItems.forEach((item) => {
     result += `
     <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="${item.link}">${item.text}</a>
-          </li>
+        <a class="nav-link active" aria-current="page" href="${item.link}">${item.text}</a>
+    </li>
     `;
   });
 
