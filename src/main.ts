8a0f2dcd663062264fe08c8f384a6e19f6a19300
Fausto E. Rodriguez <faustoerv@gmail.com>
2023-10-17 20:16:37 -0400

    crear landing page

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,5 @@
 import "./scss/main.scss";
 import "bootstrap";
-import Header from "./components/header/header.ts";
-import Intro from "./components/intro/intro.ts";
-import Specialities from "./components/specialties/specialties.ts";
-import Information from "./components/information/Information.ts";
-import Footer from "./components/footer/Footer.ts";
+import LandingPage from "./views/landingPage.ts";
 
-new Header();
-// document.querySelector<HTMLDivElement>("#header")!.innerHTML =
-//   header.getElement();
-
-new Intro();
-// document.querySelector<HTMLDivElement>("#intro")!.innerHTML =
-//   intro.getElement();
-
-new Specialities();
-
-new Information();
-
-new Footer();
+new LandingPage();
---
+++ src/views/landingPage.ts
@@ -0,0 +1,36 @@
+﻿import { BaseComponent } from "../components/baseComponent.ts";
+import Header from "../components/header/header.ts";
+import Intro from "../components/intro/intro.ts";
+import Specialities from "../components/specialties/specialties.ts";
+import Information from "../components/information/Information.ts";
+import Footer from "../components/footer/Footer.ts";
+
+export default class LandingPage extends BaseComponent {
+  constructor() {
+    const template = `
+    <section id="header"></section>
+
+    <section id="intro"></section>
+
+    <section id="specialties"></section>
+
+    <section id="information"></section>
+    
+    <section id="location"></section>
+
+    <section id="footer"></section>
+        `;
+
+    super(template, "#app");
+
+    new Header();
+
+    new Intro();
+
+    new Specialities();
+
+    new Information();
+
+    new Footer();
+  }
+}
